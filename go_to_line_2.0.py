2699b58382c66699e9a6c2d6150f2bcfa8fb1fa0
Semoyn <106271169+Semyon49@users.noreply.github.com>
2024-04-22 21:51:24 +0300

    4th algoritm(working)

--- go_to_line_2.0.py
+++ go_to_line_2.0.py
@@ -4,6 +4,7 @@ import time
 
 auv = mur.mur_init()
 speed = 10
+counter = 0
 
 # функция определения знака числа
 def zch(x): 
@@ -20,6 +21,17 @@ def y_drave(x):
     x=ogr(x)
     auv.set_motor_power(0,x)
     auv.set_motor_power(1,x)
+
+# функция движения по вертикали
+def z_drave(x): 
+    x=ogr(x)
+    auv.set_motor_power(0,x)
+    auv.set_motor_power(3,x)
+
+# функция регулировки глубине в метрах
+def kd(x): 
+    z=auv.get_depth()-x
+    z_drave((abs(z/3)**(1/3))*16*zch(z)*3)
 
 def  Find_odject(image, name):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
@@ -44,15 +56,11 @@ def  Find_odject(image, name):
             # print(x1, y1, h, w, angele)
             
             moments = cv2.moments(cnt[0])
-            x1 = moments["m10"] / moments["m00"]
-            x = moments["m10"] / (moments["m00"] * 2)
-            y1 = moments['m01'] / moments['m00']
-            y = moments['m01'] / (moments['m00'] * 2)
+            x = moments["m10"] / moments["m00"]
+            y = moments['m01'] / moments['m00']
 
-            # y = 0
 
             cv2. circle(image, (int(x), int(y)), 10, (0, 255, 0))
-            cv2. circle(image, (int(x1), int(y1)), 10, (0, 255, 255))
 
             cv2.imshow(name, image)
             cv2.waitKey(1)
@@ -60,74 +68,56 @@ def  Find_odject(image, name):
         except ZeroDivisionError:
             return None
 
-image = auv.get_image_bottom()
-height, width = image.shape[:2]
 
-while True:
-    image = auv.get_image_bottom()
-    cor = Find_odject(image)
-
-    if cor: 
-        x, y = cor
+def search_2_line(img):
+    if cor2: 
+        x, y = cor2
         controlX = 2 * (x - width / 2) / width
         controlY = 2 * (y - height / 2) / height
 
-        if abs(controlX) < 0.25:
+        if abs(controlX) < 0.2:
             y_drave(15)
 
-        elif controlX > 0.17:
-            auv.set_motor_power(1,speed / 3)
-            auv.set_motor_power(0, speed)
+        elif controlX > 0.2:
+            auv.set_motor_power(1,-speed)
+            auv.set_motor_power(0,speed)
             
-        elif controlX < -0.17:
+        elif controlX < -0.2:
             auv.set_motor_power(1,speed)
-            auv.set_motor_power(0,speed / 3)
+            auv.set_motor_power(0,-speed)
         else:
             y_drave(0)
-            
-# while True:
-#     image = auv.get_image_bottom()
-#     img1 = image[0:height//3, 0:width]
-#     img2 = image[height//3:height//3*2, 0:width]
 
-#     # cv2.imshow('1', img1)
-#     # cv2.imshow('2', img2)
-#     # cv2.waitKey(1)
-#     cor = Find_odject(img1, '123')
-#     cor = Find_odject(img2, '234')
+image = auv.get_image_bottom()
+height, width = image.shape[:2]
+         
+while True:
+    
+    if counter == 0:
+        kd(5)
+        counter = 10
+    counter -= 1
+    image = auv.get_image_bottom()
+    img1 = image[0:height//3, 0:width]
+    img2 = image[height//3:height//3*2, 0:width]
 
-    # if cor: 
-    #     x, y = cor
-    #     controlX = 2 * (x - width / 2) / width
-    #     controlY = 2 * (y - height / 2) / height
+    cor1 = Find_odject(img1, '123')
+    cor2 = Find_odject(img2, '234')
 
-    #     if abs(controlX) < 0.25:
-    #         y_drave(15)
+    if cor1: 
+        x, y = cor1
+        controlX = 2 * (x - width / 2) / width
+        controlY = 2 * (y - height / 2) / height
 
-    #     elif controlX > 0.17:
-    #         auv.set_motor_power(1,speed / 3)
-    #         auv.set_motor_power(0, speed)
+        if abs(controlX) < 0.2:
+            search_2_line(img2)
+
+        elif controlX > 0.2:
+            auv.set_motor_power(1,-speed)
+            auv.set_motor_power(0, speed)
             
-    #     elif controlX < -0.17:
-    #         auv.set_motor_power(1,speed)
-    #         auv.set_motor_power(0,speed / 3)
-    #     else:
-    #         y_drave(0)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        elif controlX < -0.2:
+            auv.set_motor_power(1,speed)
+            auv.set_motor_power(0,-speed)
+        else:
+            y_drave(0)      
