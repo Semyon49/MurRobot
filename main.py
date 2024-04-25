ac5418c6b786fd4cb7d0dc006737494c638b1e54
Semoyn <106271169+Semyon49@users.noreply.github.com>
2024-04-25 21:26:00 +0300

    Debag problem for search r and l point

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ auv = mur.mur_init()
 speed = 10
 counter = 0
 
+k = 0.15
+is_right =False
+is_left =False
+
+
 # функция определения знака числа
 def zch(x): 
     if x==0: return 1 
@@ -39,7 +44,7 @@ def  Find_odject(image, name):
 
     # значеня настраиваются в зависимости от цвета
     hsv_low = (5, 100, 100)
-    hsv_max = (150, 255, 255)
+    hsv_max = (100, 255, 255)
 
     mask = cv2.inRange(img, hsv_low, hsv_max)
     cnt, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -49,7 +54,14 @@ def  Find_odject(image, name):
         x = moments["m10"] / moments["m00"]
         y = moments['m01'] / moments['m00']
 
-        cv2. circle(image, (int(x), int(y)), 5, (0, 255, 0))
+        pt1 = int(width//2 + width * (k/2))
+        pt2 = int(width//2 - width * (k/2))
+
+        cv2.circle(image, (int(x), int(y)), 5, (255, 0, 0))
+        cv2.rectangle(image,(pt1,height),(pt2,0),(0,255,0),2)
+        cv2.line(image,(0,height//2),(width,height//2),(0,0,255),2)
+        cv2.line(image,(width//2,0),(width//2,height),(0,0,255),2)
+
 
         cv2.imshow(name, image)
         cv2.waitKey(1)
@@ -61,40 +73,48 @@ def  Find_odject(image, name):
         return None
 
 # Функция выравнивания робота по центру относительно 2/3 изображения
-def search_2_line(cor2, k=0.2):
+def search_2_line(cor2):
     if cor2: 
-        x, y = cor2
+        x, _ = cor2
         controlX = 2 * (x - width / 2) / width
 
+        
         if abs(controlX) < k:
             y_drave(15)
 
-        elif controlX > k:
+        elif controlX > k and not is_left:
             auv.set_motor_power(1,-speed)
             auv.set_motor_power(0,speed)
             
-        elif controlX < -k:
+        elif controlX < -k and not is_right:
             auv.set_motor_power(1,speed)
-            auv.set_motor_power(0,-speed)
+
         else:
             y_drave(0)
 
 # Функция выравнивания робота по центру относительно 1/3 изображения
-def search_1_line(cor1, cor2, k=0.2):
+def search_1_line(cor1, cor2):
     if cor1: 
-        x, y = cor1
+        x, _ = cor1
         controlX = 2 * (x - width / 2) / width
 
-        if abs(controlX) < k:
+        if abs(controlX) < 0.05:
+            is_left = False
+            is_right = False
+
+        elif abs(controlX) < k:
             search_2_line(cor2)
 
         elif controlX > k:
+            is_right = True
             auv.set_motor_power(1,-speed)
             auv.set_motor_power(0, speed)
             
         elif controlX < -k:
+            is_left = True
             auv.set_motor_power(1,speed)
             auv.set_motor_power(0,-speed)
+
         else:
             y_drave(0)
 
@@ -114,8 +134,10 @@ while True:
     dive()
 
     image = auv.get_image_bottom()
-    img1 = image[0:height//3, 0:width]
-    img2 = image[height//3:height//3*2, 0:width]
+    img1 = image[:height//3, 0:width]
+    # img2 = image[height//3:height, 0:width]
+    img2 = image[height//3:height//3*2, :]
+
 
     cor1 = Find_odject(img1, 'CCV1')
     cor2 = Find_odject(img2, 'CCV2')
